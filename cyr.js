866e7c11ca11adc9e21f37fced5f2c11b714d5c7
nickkolok <nickkolok@mail.ru>
2016-07-18 13:53:58 +0300

    GPLv3 copyright notice

--- cyr.js
+++ cyr.js
@@ -1,3 +1,41 @@
+/*
+Copyright Nikolay Avdeev aka NickKolok aka Николай Авдеев 2016
+
+Всем привет из снежного Воронежа!
+
+This file is part of w2l-napilnik.
+
+    w2l-napilnik is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    w2l-napilnik is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with w2l-napilnik.  If not, see <http://www.gnu.org/licenses/>.
+
+  (Этот файл — часть w2l-napilnik.
+
+   w2l-napilnik - свободная программа: вы можете перераспространять её и/или
+   изменять её на условиях Стандартной общественной лицензии GNU в том виде,
+   в каком она была опубликована Фондом свободного программного обеспечения;
+   либо версии 3 лицензии, либо (по вашему выбору) любой более поздней
+   версии.
+
+   w2l-napilnik распространяется в надежде, что она будет полезной,
+   но БЕЗО ВСЯКИХ ГАРАНТИЙ; даже без неявной гарантии ТОВАРНОГО ВИДА
+   или ПРИГОДНОСТИ ДЛЯ ОПРЕДЕЛЕННЫХ ЦЕЛЕЙ. Подробнее см. в Стандартной
+   общественной лицензии GNU.
+
+   Вы должны были получить копию Стандартной общественной лицензии GNU
+   вместе с этой программой. Если это не так, см.
+   <http://www.gnu.org/licenses/>.)
+*/
+
 // В который раз спасибо http://dxdy.ru/topic18924-15.html
 
 module.exports.cyrtable={
